a300d1da9bf5f6d7b636014e9d8028459792a775
ChristopheLagaillarde <chrislag94@gmail.com>
2022-04-01 11:01:20 +0400

    test 0 bug SonarCloud5

--- src/casBibliotheque/CasBibliotheque.java
+++ src/casBibliotheque/CasBibliotheque.java
@@ -3,7 +3,7 @@ package casbibliotheque;
 public class CasBibliotheque {
 
 	public static void main(String[] args) {
-		System.out.println("Hello SonarCloud");
+		System.out.println("Hello");
 	}
 
 }
